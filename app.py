79051566e5a114bea0af5347de6d2581b489d70e
jyi <joshua.yi@amadeus.com>
2020-09-25 13:43:13 -0400

    Add POST api

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
 # export FLASK_APP=app.py
+# export FLASK_ENV=development
 # export FLASK_RUN_PORT=5000
+# 200 - Success
+# 404 - Not found
 # 405 - Not allowed
 @app.route('/')
 def hello_world():
@@ -24,6 +27,26 @@ Therefore, an image/video of a page could be :
 
 This is why we use JSON to communicate
 """
+
+
+@app.route('/add_two_nums', methods=["POST"])  # Could add "GET" to methods if we wanted to allow
+def add_two_nums():
+    # Get x, y from post data
+    dataDict = request.get_json()
+    if 'y' not in dataDict:
+        return 'ERROR', 305
+    x = dataDict['x']
+    y = dataDict['y']
+
+    # add z = x + y
+    z = x + y
+
+    # Prepare a JSON
+    retJSON = {
+        'z': z
+    }
+    # return jsonifyied json
+    return jsonify(retJSON), 200
 
 
 @app.route('/bye')
