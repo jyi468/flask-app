ebf4742c386e3c7610f75eaa3bed4e38ff8c068a
jyi <joshua.yi@amadeus.com>
2020-09-25 13:23:06 -0400

    Add files

---
+++ app.py
@@ -0,0 +1,44 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+# export FLASK_APP=app.py
+# export FLASK_RUN_PORT=5000
+# 405 - Not allowed
+@app.route('/')
+def hello_world():
+    return "Hello World!"
+
+
+@app.route('/hithere')  # Return resource at route hithere
+def hi_there_everyone():
+    return 'I just hit /hithere'
+
+
+"""
+All communications between server/server, server/browser, browser/browser can only be done via TEXT 
+Can send representations of image, videos via text but can't actually send that image
+This is because of TCP
+Therefore, an image/video of a page could be :
+- 2d array text of RGB values
+
+This is why we use JSON to communicate
+"""
+
+
+@app.route('/bye')
+def bye():
+    # Prepare response for request that came to /bye
+    # Return usually string, JSON, page
+    c = 2*123
+    s = str(c)
+    # c = 1/0
+    retJson = {
+        'field1': 'abc',
+        'field2': 'def'
+    }
+    return jsonify(retJson)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=6000)  # important to set this to true to debug any errors
